6d149e213de71142d249efc5352fd37bc173a086
tahira_17 <tahiranawaz12345@gmail.com>
2020-06-18 17:03:26 +0500

    final commit

---
+++ src/main/resources/static/precache-manifest.9b400fb7ddd515f3a511cfdbef90603a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ee9b78c80bc6cbfec1e9ebc526bac2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3438bf1d232ecb7720b2",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "4c3510e437d4e7af22bd",
+    "url": "/static/css/main.00d4352e.chunk.css"
+  },
+  {
+    "revision": "3438bf1d232ecb7720b2",
+    "url": "/static/js/2.d79185f1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.d79185f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c3510e437d4e7af22bd",
+    "url": "/static/js/main.9c16608f.chunk.js"
+  },
+  {
+    "revision": "6d14a0cf0bac0675ec53",
+    "url": "/static/js/runtime-main.859993c1.js"
+  }
+]);
